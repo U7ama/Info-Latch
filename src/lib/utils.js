cffbf2e0b85ff85cc7655041b5b96702053e5c31
Usama Aslam <61847858+U7ama@users.noreply.github.com>
2024-06-26 18:13:23 +0000

    ch

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,16 +2,18 @@ import mongoose from "mongoose";
 
 const connection = {};
 
-export const connectToDb =async () => {
-    try {
-        if (connection.isConnected) {
-            console.log("Using existing connection");
-            return;
-        }
-        const db = await mongoose.connect(process.env.MONGO);
-        connection.isConnected = db.connections[0].readyState;
-    } catch (error) {
-        console.log(error);
-        throw new Error(error);
+export const connectToDb = async () => {
+  try {
+    if (connection.isConnected) {
+      console.log("Using existing connection");
+      return;
     }
-}
+    const db = await mongoose.connect(
+      "mongodb+srv://usama:usama@cluster0.iqyds.mongodb.net/InfoLatch?retryWrites=true&w=majority"
+    );
+    connection.isConnected = db.connections[0].readyState;
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
