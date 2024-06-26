1114f6a568f0d07826d760cdda54fae44eb66a44
U7ama <u7amajutt@gmail.com>
2024-06-27 00:30:55 +0500

    blogs

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,29 +1,30 @@
-import React from 'react'
-import styles from "./blog.module.css"
-import PostCard from '@/components/postCard/postCard';
+import React from "react";
+import styles from "./blog.module.css";
+import PostCard from "@/components/postCard/postCard";
 
 const getData = async () => {
-  const res = await fetch("https://next-js-14-blog-app.vercel.app/api/blog", {next:{revalidate:3600}});
+  const res = await fetch("http://localhost:3000/api/blog", {
+    next: { revalidate: 3600 },
+  });
 
   if (!res.ok) {
     throw new Error("Something went wrong");
   }
 
   return res.json();
-}
+};
 const BlogPage = async () => {
-
   const posts = await getData();
 
   return (
     <div className={styles.container}>
       {posts.map((post) => (
         <div className={styles.post} key={post.id}>
-          <PostCard post={post}/>
+          <PostCard post={post} />
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default BlogPage
+export default BlogPage;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,13 @@
-
-'use client'
-import React,{useState} from 'react'
+"use client";
+import React, { useState } from "react";
 import styles from "./navbar.module.css";
-import Link from 'next/link';
+import Link from "next/link";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
-import Megadropdown from '../MegaDropDown';
-import MegaDropDownCompany from '../MegadropdownForCompany';
-import MegaDropDownIndustries from '../MegaDropDownForIndustries';
-const Navbar =  () => {
+import Megadropdown from "../MegaDropDown";
+import MegaDropDownCompany from "../MegadropdownForCompany";
+import MegaDropDownIndustries from "../MegaDropDownForIndustries";
+import Image from "next/image";
+const Navbar = () => {
   const [hovered, setHovered] = useState(false);
 
   const handleMouseEnter = (item) => {
@@ -18,67 +18,114 @@ const Navbar =  () => {
     setHovered(false);
   };
 
-
   return (
-    <div className={`flex justify-between py-6 px-28 fixed w-full font-poppins text-[15px] hrefp-0 left-0 right-0 z-10 text-primary bg-white border-b `}>
+    <div
+      className={`flex justify-between py-6 px-28 fixed w-full font-poppins text-[15px] hrefp-0 left-0 right-0 z-10 text-primary bg-white border-b `}
+    >
+      <Link href={"/"}>
         <div>
-        <img src="/Logoinfo.png" alt="LOGO" width={130} />
+          <Image src="/Logoinfo.png" alt="LOGO" width={130} height={100} />
         </div>
-        <div>
-          <ul className='flex gap-6 font-semibold'>
-            <Link href={"/"} onMouseEnter={() => handleMouseEnter("home")}
-              onMouseLeave={handleMouseLeave}
-            >Home</Link>
-            <li onMouseEnter={() => handleMouseEnter("services")} onMouseLeave={handleMouseLeave}>
-            <Link href="/" className='flex gap-2 justify-center items-center transition-all duration-150'>
-              Services {hovered === "services" ? <IoIosArrowUp /> : <IoIosArrowDown />}
+      </Link>
+      <div>
+        <ul className="flex gap-6 font-semibold">
+          <Link
+            href={"/"}
+            onMouseEnter={() => handleMouseEnter("home")}
+            onMouseLeave={handleMouseLeave}
+          >
+            Home
+          </Link>
+          <li
+            onMouseEnter={() => handleMouseEnter("services")}
+            onMouseLeave={handleMouseLeave}
+          >
+            <Link
+              href="/"
+              className="flex gap-2 justify-center items-center transition-all duration-150"
+            >
+              Services{" "}
+              {hovered === "services" ? <IoIosArrowUp /> : <IoIosArrowDown />}
             </Link>
             {hovered === "services" && (
-              <div onMouseEnter={() => handleMouseEnter("services")} onMouseLeave={handleMouseLeave}>
-                  <Megadropdown/>
+              <div
+                onMouseEnter={() => handleMouseEnter("services")}
+                onMouseLeave={handleMouseLeave}
+              >
+                <Megadropdown />
               </div>
-          
             )}
           </li>
           <li>
-            <Link href={"/"} onMouseEnter={() => handleMouseEnter("Industries")}
+            <Link
+              href={"/"}
+              onMouseEnter={() => handleMouseEnter("Industries")}
               onMouseLeave={handleMouseLeave}
-              className='flex gap-2 justify-center items-center transition-all duration-150'
-            >Industries {hovered === "Industries" ? <IoIosArrowUp /> : <IoIosArrowDown />} </Link>
-             {hovered === "Industries" && (
-              <div onMouseEnter={() => handleMouseEnter("Industries")} onMouseLeave={handleMouseLeave}>
-                  <MegaDropDownIndustries/>
+              className="flex gap-2 justify-center items-center transition-all duration-150"
+            >
+              Industries{" "}
+              {hovered === "Industries" ? <IoIosArrowUp /> : <IoIosArrowDown />}{" "}
+            </Link>
+            {hovered === "Industries" && (
+              <div
+                onMouseEnter={() => handleMouseEnter("Industries")}
+                onMouseLeave={handleMouseLeave}
+              >
+                <MegaDropDownIndustries />
               </div>
-          
             )}
           </li>
-            
-            <Link href={"/"} onMouseEnter={() => handleMouseEnter("work")}
+
+          <Link
+            href={"/"}
+            onMouseEnter={() => handleMouseEnter("work")}
+            onMouseLeave={handleMouseLeave}
+          >
+            Our Work
+          </Link>
+          <li>
+            <Link
+              href={"/"}
+              onMouseEnter={() => handleMouseEnter("Company")}
               onMouseLeave={handleMouseLeave}
-            >Our Work</Link>
-            <li>
-            <Link href={"/"} onMouseEnter={() => handleMouseEnter("Company")}
-              onMouseLeave={handleMouseLeave}
-              className='flex gap-2 justify-center items-center transition-all duration-150'
-            >Company {hovered === "Company" ? <IoIosArrowUp /> : <IoIosArrowDown />} </Link>
-             {hovered === "Company" && (
-              <div onMouseEnter={() => handleMouseEnter("Company")} onMouseLeave={handleMouseLeave}>
-                  <MegaDropDownCompany/>
+              className="flex gap-2 justify-center items-center transition-all duration-150"
+            >
+              Company{" "}
+              {hovered === "Company" ? <IoIosArrowUp /> : <IoIosArrowDown />}{" "}
+            </Link>
+            {hovered === "Company" && (
+              <div
+                onMouseEnter={() => handleMouseEnter("Company")}
+                onMouseLeave={handleMouseLeave}
+              >
+                <MegaDropDownCompany />
               </div>
-          
             )}
-            </li>
-           
-            <Link href={"/"} onMouseEnter={() => handleMouseEnter("Blog")}
-              onMouseLeave={handleMouseLeave}
-              className={`${hovered === "Blog" ? 'underline' : ''}`}
-            >Blog</Link>
-          </ul>
-        </div>
-        <button className='border-2 bg-tirtry text-secondary font-semibold rounded-full py-3 px-6 text-sm'>Contact US</button>
+          </li>
+
+          <Link
+            href={"/blog"}
+            onMouseEnter={() => handleMouseEnter("Blog")}
+            onMouseLeave={handleMouseLeave}
+            className={`${hovered === "Blog" ? "underline" : ""}`}
+          >
+            Blog
+          </Link>
+          <Link
+            href={"/admin"}
+            // onMouseEnter={() => handleMouseEnter("Blog")}
+            // onMouseLeave={handleMouseLeave}
+            // className={`${hovered === "Blog" ? "underline" : ""}`}
+          >
+            Add Blog
+          </Link>
+        </ul>
       </div>
+      <button className="border-2 bg-tirtry text-secondary font-semibold rounded-full py-3 px-6 text-sm">
+        Contact US
+      </button>
+    </div>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
