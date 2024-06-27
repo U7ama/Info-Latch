73b3b96cd43593dd23b1603dfb4063923a4009c4
U7ama <u7amajutt@gmail.com>
2024-06-27 23:33:20 +0500

    changes

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,15 +1,19 @@
-import { auth } from '@/lib/auth'
-import React, { Suspense } from 'react'
+import { auth } from "@/lib/auth";
+import React, { Suspense } from "react";
 import styles from "./admin.module.css";
-import AdminPosts from '@/components/adminPosts/adminPosts';
-import AdminPostForm from '@/components/adminPostForm/adminPostForm';
-import AdminUsers from '@/components/adminUsers/adminUsers';
-import AdminUserForm from '@/components/adminUserForm/adminUserForm';
-import AdminContact from '@/components/adminContact/adminContact';
+import AdminPosts from "@/components/adminPosts/adminPosts";
+import AdminPostForm from "@/components/adminPostForm/adminPostForm";
+import AdminUsers from "@/components/adminUsers/adminUsers";
+import AdminUserForm from "@/components/adminUserForm/adminUserForm";
+import AdminContact from "@/components/adminContact/adminContact";
+import { redirect } from "next/navigation";
 
 const AdminPage = async () => {
-    const session = await auth();
-     
+  const session = await auth();
+  console.log("session", session);
+  if (!session) {
+    redirect("/login");
+  }
   return (
     <div className={styles.container}>
       <div className={styles.row}>
@@ -19,13 +23,11 @@ const AdminPage = async () => {
             </Suspense>
         </div> */}
         <div className={styles.col}>
-            <AdminPostForm userId={session?.user?.id} />
+          <AdminPostForm userId={session?.user?.id} />
         </div>
       </div>
-     
-     
     </div>
-  )
-}
+  );
+};
 
-export default AdminPage
+export default AdminPage;
