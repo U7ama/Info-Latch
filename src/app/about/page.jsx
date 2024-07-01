84b3ace65ca1b46edfcbbef4aab584ea16460d3a
U7ama <u7amajutt@gmail.com>
2024-07-01 16:39:13 +0500

    build fixes

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,68 +1,107 @@
-import React from 'react'
+import React from "react";
 
-import GetandEstimateButtn from '@/components/GetandEstimateButtn';
-import BookACallButton from '@/components/BookACallButton';
+import GetandEstimateButtn from "@/components/GetandEstimateButtn";
+import BookACallButton from "@/components/BookACallButton";
 export const metadata = {
-    title: "About Page",
-    description: "About description",
+  title: "About Page",
+  description: "About description",
 };
-import GetConsultingSection from '@/components/GetConsultingSection';
-import BenefitsUs from '../services/BenefitsWorkWithUs';
+import GetConsultingSection from "@/components/GetConsultingSection";
+import BenefitsUs from "../services/BenefitsWorkWithUs";
+import Image from "next/image";
 const AboutPage = () => {
   return (
     <>
-    <div className='bg-forth'>
-    <div className="  flex justify-between pt-20 pb-10 gap-10">
-    <div className='pt-20 min-h-fit flex justify-center items-start  text-secondary flex-col py-20 pl-20 w-[70%]'>
-        <h1 className=' text-6xl  font-poppins tracking-wide font-bold leading-tight'>Find out more about
-        our company</h1>
-        <p className=' font-poppins pt-6'>Continued success at dotcode has prompted us to grow an international team of the brightest minds in software development. Meet the company’s leadership and the hard-working people, who deliver game-changing software solutions to companies like yours.</p>
-        <div  
-  className=' py-6 flex gap-4 justify-start items-center'>
-<BookACallButton/>
-<GetandEstimateButtn/>
-</div>
-    </div>
-   
-    </div>
-   
-    </div>
-   <div>
-    <img src="/group.jpg" alt="helo" className=' w-full minh-screen' />
-   </div>
-   <div className=' flex justify-between items-start py-20 px-28 gap-10 min-h-screen w-full'>
-   <div className=' w-1/2'>
-    <h1 className=' text-4xl font-heading mb-8 font-semibold uppercase text-forth'>Our story </h1>
-    <p>Established in 2017 by a group of brilliant minds, experts in Computer Sciences and Applied Mathematics, dotcode embarked on a journey to redefine the future of software development for those who are in need of functional yet cost-effective products. From the very beginning, the company set its sights high, striving not just for success but for excellence. This commitment has been the driving force behind the dotcode's consistent growth. Year after year, we have surpassed expectations, leaving a trail of satisfied clients and great projects in our wake. Based on our experience, we know exactly how to combine software development according to the rules and best practices without sacrificing the innovativeness of your bold ideas.
-      <br />
-      <br />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda non impedit quasi vitae necessitatibus reprehenderit voluptatibus corporis qui, animi est, maiores ullam, dignissimos officia atque accusantium quos. Repellat, amet obcaecati!
+      <div className="bg-forth">
+        <div className="  flex justify-between pt-20 pb-10 gap-10">
+          <div className="pt-20 min-h-fit flex justify-center items-start  text-secondary flex-col py-20 pl-20 w-[70%]">
+            <h1 className=" text-6xl  font-poppins tracking-wide font-bold leading-tight">
+              Find out more about our company
+            </h1>
+            <p className=" font-poppins pt-6">
+              Continued success at dotcode has prompted us to grow an
+              international team of the brightest minds in software development.
+              Meet the company’s leadership and the hard-working people, who
+              deliver game-changing software solutions to companies like yours.
+            </p>
+            <div className=" py-6 flex gap-4 justify-start items-center">
+              <BookACallButton />
+              <GetandEstimateButtn />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <Image src="/group.jpg" alt="helo" className=" w-full minh-screen" />
+      </div>
+      <div className=" flex justify-between items-start py-20 px-28 gap-10 min-h-screen w-full">
+        <div className=" w-1/2">
+          <h1 className=" text-4xl font-heading mb-8 font-semibold uppercase text-forth">
+            Our story{" "}
+          </h1>
+          <p>
+            Established in 2017 by a group of brilliant minds, experts in
+            Computer Sciences and Applied Mathematics, dotcode embarked on a
+            journey to redefine the future of software development for those who
+            are in need of functional yet cost-effective products. From the very
+            beginning, the company set its sights high, striving not just for
+            success but for excellence. This commitment has been the driving
+            force behind the dotcode&apos;s consistent growth. Year after year,
+            we have surpassed expectations, leaving a trail of satisfied clients
+            and great projects in our wake. Based on our experience, we know
+            exactly how to combine software development according to the rules
+            and best practices without sacrificing the innovativeness of your
+            bold ideas.
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            non impedit quasi vitae necessitatibus reprehenderit voluptatibus
+            corporis qui, animi est, maiores ullam, dignissimos officia atque
+            accusantium quos. Repellat, amet obcaecati!
+          </p>
+        </div>
+        <div className=" w-1/2">
+          <Image src="/single.jpg" alt="helo" />
+        </div>
+      </div>
+      <div className=" flex justify-between items-start py-20 px-28 gap-10 min-h-screen w-full">
+        <div className=" w-1/2">
+          <Image src="/group.jpg" alt="helo" />
+        </div>
+        <div className=" w-1/2">
+          <h1 className=" text-4xl font-heading mb-8 font-semibold uppercase text-forth">
+            Our Mission{" "}
+          </h1>
+          <p>
+            Established in 2017 by a group of brilliant minds, experts in
+            Computer Sciences and Applied Mathematics, dotcode embarked on a
+            journey to redefine the future of software development for those who
+            are in need of functional yet cost-effective products. From the very
+            beginning, the company set its sights high, striving not just for
+            success but for excellence. This commitment has been the driving
+            force behind the dotcode&apos;s consistent growth. Year after year,
+            we have surpassed expectations, leaving a trail of satisfied clients
+            and great projects in our wake. Based on our experience, we know
+            exactly how to combine software development according to the rules
+            and best practices without sacrificing the innovativeness of your
+            bold ideas.
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            non impedit quasi vitae necessitatibus reprehenderit voluptatibus
+            corporis qui, animi est, maiores ullam, dignissimos officia atque
+            accusantium quos. Repellat, amet obcaecati!
+          </p>
+        </div>
+      </div>
+      <BenefitsUs />
+      <GetConsultingSection
+        heading={" Interested in different services?"}
+        text1={"Look no further. Your challenge, our expertise."}
+        text2={"Schedule a call to share your idea!"}
+      />
+    </>
+  );
+};
 
-    </p>
-   </div>
-   <div className=' w-1/2'>
-   <img src="/single.jpg" alt="helo" />
-   </div>
-   </div>
-   <div className=' flex justify-between items-start py-20 px-28 gap-10 min-h-screen w-full'>
-   <div className=' w-1/2'>
-   <img src="/group.jpg" alt="helo" />
-   </div>
-   <div className=' w-1/2'>
-    <h1 className=' text-4xl font-heading mb-8 font-semibold uppercase text-forth'>Our Mission </h1>
-    <p>Established in 2017 by a group of brilliant minds, experts in Computer Sciences and Applied Mathematics, dotcode embarked on a journey to redefine the future of software development for those who are in need of functional yet cost-effective products. From the very beginning, the company set its sights high, striving not just for success but for excellence. This commitment has been the driving force behind the dotcode's consistent growth. Year after year, we have surpassed expectations, leaving a trail of satisfied clients and great projects in our wake. Based on our experience, we know exactly how to combine software development according to the rules and best practices without sacrificing the innovativeness of your bold ideas.
-      <br />
-      <br />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda non impedit quasi vitae necessitatibus reprehenderit voluptatibus corporis qui, animi est, maiores ullam, dignissimos officia atque accusantium quos. Repellat, amet obcaecati!
-
-    </p>
-   </div>
-   
-   </div>
-   <BenefitsUs/>
-   <GetConsultingSection heading={" Interested in different services?"} text1={"Look no further. Your challenge, our expertise."} text2={"Schedule a call to share your idea!"}/>
-   </>
-  )
-}
-
-export default AboutPage
+export default AboutPage;
--- src/components/Home/CloudSection.jsx
+++ src/components/Home/CloudSection.jsx
@@ -35,41 +35,61 @@ const CloudSection = () => {
                   <h2 className="text-3xl font-bold px-10 pb-4 capitalize font-heading text-forth">
                     {" "}
                     Software development
-
                   </h2>
                   <p className=" px-20 text-sm">
-                  Info Latch is an expert at creating custom software that is reliable, scalable, and appropriately matched to your company's specific needs. Whether you require enterprise software, integrated solutions, or a custom application, our experience promises excellent outcomes that advance your company. 
-
+                    Info Latch is an expert at creating custom software that is
+                    reliable, scalable, and appropriately matched to your
+                    company&apos;s specific needs. Whether you require
+                    enterprise software, integrated solutions, or a custom
+                    application, our experience promises excellent outcomes that
+                    advance your company.
                   </p>
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className=" flex justify-center items-center flex-col py-20">
                   <h2 className="text-3xl font-bold px-10 pb-4 capitalize font-heading text-forth">
-                  Digital design
+                    Digital design
                   </h2>
                   <p className=" px-20 text-sm">
-                  With careful design of interfaces and an appealing style, we strive to create smooth and captivating user experiences with our digital design services. We place a high value on accessibility and usability to ensure that every digital interaction on web and mobile platforms matches your business identity and improves the user experience.
+                    With careful design of interfaces and an appealing style, we
+                    strive to create smooth and captivating user experiences
+                    with our digital design services. We place a high value on
+                    accessibility and usability to ensure that every digital
+                    interaction on web and mobile platforms matches your
+                    business identity and improves the user experience.
                   </p>
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className=" flex justify-center items-center flex-col py-20">
                   <h2 className="text-3xl font-bold px-10 pb-4 capitalize font-heading text-forth">
-                  AI solution
+                    AI solution
                   </h2>
                   <p className=" px-20 text-sm">
-                  Info Latch creates innovative solutions by utilizing artificial intelligence to automate procedures, analyze trends in data, and provide knowledge that may be used. Our AI solutions enable businesses to optimize processes, identify trends, and make confident decisions based on data, ranging from machine learning techniques to natural language processing tools.
+                    Info Latch creates innovative solutions by utilizing
+                    artificial intelligence to automate procedures, analyze
+                    trends in data, and provide knowledge that may be used. Our
+                    AI solutions enable businesses to optimize processes,
+                    identify trends, and make confident decisions based on data,
+                    ranging from machine learning techniques to natural language
+                    processing tools.
                   </p>
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className=" flex justify-center items-center flex-col py-20">
                   <h2 className="text-3xl font-bold px-10 pb-4 capitalize font-heading text-forth">
-                  Cloud solution
+                    Cloud solution
                   </h2>
                   <p className=" px-20 text-sm">
-                  Reliable cloud solutions from Info Latch are designed to improve scalability and optimize your company's operations. Our expertise ensures secure, adaptable, and affordable solutions that support your growth and creative goals in today's digital ecosystem, whether you require help with cloud migration, infrastructure management, or SaaS implementation.
+                    Reliable cloud solutions from Info Latch are designed to
+                    improve scalability and optimize your company&apos;s
+                    operations. Our expertise ensures secure, adaptable, and
+                    affordable solutions that support your growth and creative
+                    goals in today&apos;s digital ecosystem, whether you require
+                    help with cloud migration, infrastructure management, or
+                    SaaS implementation.
                   </p>
                 </div>
               </SwiperSlide>
